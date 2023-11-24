b458d208c7c15fd0b0c5da3e3f118f39357ea51b
Krzysztof Pempera <krzysztof.pempera@tietoevry.com>
2023-11-24 15:56:41 +0100

    Add get event by id

--- ToDo/routes/events.js
+++ ToDo/routes/events.js
@@ -1,11 +1,43 @@
 import express from "express";
 import db from "../db/service.js";
+import { ObjectId } from "mongodb";
 const eventRouter = express.Router();
 eventRouter.get('/', async (req, res) => {
-    let collection = await db.collection("events");
-    let result = await collection.find({})
+    let collection = await db.collection("Events");
+    const eventId = req.query.eventId;
+    let result = (await collection.aggregate([
+        {
+            $match: {
+                _id: new ObjectId(eventId)
+            }
+        },
+        {
+            $lookup: {
+                from: "Tags",
+                localField: "tags",
+                foreignField: "_id",
+                as: "tags"
+            }
+        }
+    ])
         .limit(50)
-        .toArray();
+        .toArray());
+    res.send(result).status(200);
+});
+eventRouter.get('/', async (req, res) => {
+    let collection = await db.collection("Events");
+    let result = (await collection.aggregate([
+        {
+            $lookup: {
+                from: "Tags",
+                localField: "tags",
+                foreignField: "_id",
+                as: "tags"
+            }
+        }
+    ])
+        .limit(50)
+        .toArray());
     res.send(result).status(200);
 });
 export default eventRouter;
--- ToDo/routes/events.ts
+++ ToDo/routes/events.ts
@@ -1,16 +1,56 @@
 import express from "express";
 import db from "../db/service.js";
 
+import { ObjectId } from "mongodb";
+import Event from "../models/event.js";
+
 const eventRouter = express.Router();
 
 eventRouter.get('/', async (req: express.Request, res: express.Response) => {
-    let collection = await db.collection("events");
+    let collection = await db.collection("Events");
+    const eventId = req.query.eventId as string;
 
-    let result = await collection.find({})
+    let result = (await collection.aggregate([
+        {
+            $match: {
+                _id: new ObjectId(eventId)
+            }
+        },
+        {
+            $lookup: {
+                from: "Tags",
+                localField: "tags",
+                foreignField: "_id",
+                as: "tags"
+            }
+        }
+    ])
         .limit(50)
-        .toArray();
+        .toArray()) as unknown as Event[];
 
     res.send(result).status(200);
 });
+
+
+eventRouter.get('/', async (req: express.Request, res: express.Response) => {
+    let collection = await db.collection("Events");
+
+    let result = (await collection.aggregate([
+        {
+            $lookup: {
+                from: "Tags",
+                localField: "tags",
+                foreignField: "_id",
+                as: "tags"
+            }
+        }
+    ])
+        .limit(50)
+        .toArray()) as unknown as Event[];
+
+        res.send(result).status(200);
+});
+
+
 
 export default eventRouter;
